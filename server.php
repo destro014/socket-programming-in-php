d4b109bc315ef1f27352f3545567016e3bbdbaf6
Destro <destro014@users.noreply.github.com>
2018-04-07 16:39:14 +0545

    waiting for connection added

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php 
-	$url1=$_SERVER['REQUEST_URI'];
-    	header("Refresh: 5; URL=$url1");
+    $url1=$_SERVER['REQUEST_URI'];
+    header("Refresh: 5; URL=$url1");
 	// set some variables
 	 $host = "127.0.0.1";
 	 $port = 25003;
@@ -26,12 +26,12 @@
 			<?php 
 				// create socket
 				$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-
+				do {
 				// bind socket to port
 				$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-
 				// start listening for connections
 				$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+				}while(!$result);
 				// accept incoming connections
 			 	// spawn another socket to handle communication
 			 	$spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
